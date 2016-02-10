2d3a6fb654678ed57de7e66101b6161decf6cffe
Fahad Qazi <fahadqazi@gmail.com>
2016-02-10 02:59:02 +0000

    just some files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+	var car = {
+			make: "VW",
+			type: "polo",
+			color: "blue",
+			isTurnedOn: false,
+			numWheels: 4,
+			seats: ["seat1","seat2","seat3","seat4"],
+			turnOn: function(){
+				this.isTurnedOn = true;
+			},
+			fly: function(){
+				alert("Come on man! I can't fly!");
+			}
+		}
+
+		console.log("hey there!")
+
+function sayMyName(){
+	alert("hello Fahad");
+}
